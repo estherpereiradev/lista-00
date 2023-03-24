be50b8d79a19cdf535e9be7c9d03d699efa0f7c1
G0ld3N-GoblinK <88694415+G0ld3N-GoblinK@users.noreply.github.com>
2023-03-24 00:19:26 -0300

    Add files via upload

---
+++ q_21.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+main(){
+	
+	float numero;
+	
+	printf("Insira o numero");
+	scanf("%d", &numero);
+	
+	if (numero > 0){
+		printf("O numero e positivo");
+		
+	}
+	
+	printf("O numero e negativo");	
+}
